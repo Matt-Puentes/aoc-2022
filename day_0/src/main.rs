c8b30a67f0c1a63522fedbbf52979640ed4ee44e
Matt Puentes <mpuentes@mitre.org>
2022-12-01 13:03:30 -0500

    init commit

---
+++ day_0/src/main.rs
@@ -0,0 +1,38 @@
+use std::env;
+
+fn pt_1(str_input: &str) -> usize {
+    str_input.len().try_into().unwrap_or(0)
+}
+
+fn pt_2(str_input: &str) -> usize {
+    str_input.len().try_into().unwrap_or(0)
+}
+
+fn main() {
+    println!("Running day 0");
+    parse_args();
+}
+exa
+fn parse_args() {
+    let args: Vec<String> = env::args().collect();
+    let text = if args.len() > 2 && args[2].eq("example") {
+        include_str!("./example.txt")
+    } else {
+        include_str!("./input.txt")
+    };
+
+    if args.len() < 2 {
+        println!("Part 1 result: {}", pt_1(text));
+        println!("Part 2 result: {}", pt_2(text));
+    } else {
+        match &args[1].parse() {
+            Ok(1) => println!("Part 1 result: {}", pt_1(text)),
+            Ok(2) => println!("Part 2 result: {}", pt_2(text)),
+            Ok(0) => {
+                println!("Part 1 result: {}", pt_1(text));
+                println!("Part 2 result: {}", pt_2(text));
+            }
+            _ => panic!("invalid test number"),
+        }
+    }
+}
---
+++ setup.sh
@@ -0,0 +1,6 @@
+[ -d "day_$1" ] && echo "Directory day_$1 exists." && exit
+
+cp -r day_0 day_$1
+sed -i '' "s/day_0/day_$1/g" day_$1/Cargo.toml
+sed -i '' "s/day 0/day $1/g" day_$1/src/main.rs
+code day_$1
