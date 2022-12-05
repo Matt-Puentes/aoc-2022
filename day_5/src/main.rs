6e1399d3b1a372b485b2bc0559d133254be10098
Matt Puentes <mpuentes@mitre.org>
2022-12-05 10:56:09 -0500

    Day 5 complete

---
+++ day_5/src/main.rs
@@ -0,0 +1,93 @@
+mod parse_args;
+use core::panic;
+use std::vec;
+
+use parse_args::{parse_args, Test};
+
+fn build_stacks(boxes: &str) -> Vec<Vec<char>> {
+    let mut boxes = boxes.lines().rev();
+    // build vector of stacks
+    let mut stacks: Vec<Vec<char>> = vec![];
+    for c in boxes.next().unwrap().chars() {
+        if c != ' ' {
+            stacks.push(Vec::new());
+        }
+    }
+
+    for l in boxes {
+        let mut chars = l.chars();
+        for i in 0..stacks.len() {
+            chars.next();
+            match chars.next() {
+                Some(' ') => (),
+                Some(c) => stacks.get_mut(i).unwrap().push(c),
+                _ => panic!("No char found"),
+            }
+            chars.next();
+            // throw away space between lines
+            chars.next();
+        }
+    }
+    stacks
+}
+
+fn pt_1(str_input: &str) {
+    let (boxes, instructions) = str_input.split_once("\n\n").unwrap();
+    let mut stacks = build_stacks(boxes);
+    for instruction in instructions.lines() {
+        let mut chars = instruction.split(' ');
+        let move_amt = chars.nth(1).unwrap().parse::<usize>().unwrap();
+        let from_stack = chars.nth(1).unwrap().parse::<usize>().unwrap() as usize;
+        let to_stack = chars.nth(1).unwrap().parse::<usize>().unwrap() as usize;
+        for _ in 0..move_amt {
+            let c = stacks
+                .get_mut(from_stack - 1)
+                .unwrap()
+                .pop()
+                .expect("Stack is empty");
+            stacks.get_mut(to_stack - 1).unwrap().push(c)
+        }
+    }
+    println!(
+        "Part 1 result: {}",
+        stacks
+            .iter_mut()
+            .map(|s| s.last().unwrap())
+            .collect::<String>()
+    )
+}
+
+fn pt_2(str_input: &str) {
+    let (boxes, instructions) = str_input.split_once("\n\n").unwrap();
+    let mut stacks = build_stacks(boxes);
+    for instruction in instructions.lines() {
+        let mut chars = instruction.split(' ');
+        let move_amt = chars.nth(1).unwrap().parse::<usize>().unwrap();
+        let from_stack = chars.nth(1).unwrap().parse::<usize>().unwrap() as usize;
+        let to_stack = chars.nth(1).unwrap().parse::<usize>().unwrap() as usize;
+        let from_vec = stacks.get_mut(from_stack - 1).unwrap();
+        let t = from_vec
+            .drain(from_vec.len() - (move_amt)..)
+            .collect::<Vec<_>>();
+        stacks.get_mut(to_stack - 1).unwrap().extend(t);
+    }
+    println!(
+        "Part 2 result: {}",
+        stacks
+            .iter_mut()
+            .map(|s| s.last().unwrap())
+            .collect::<String>()
+    )
+}
+
+fn main() {
+    println!("Running day 5");
+    match parse_args() {
+        Test::One(text) => pt_1(text),
+        Test::Two(text) => pt_2(text),
+        Test::Both(text) => {
+            pt_1(text);
+            pt_2(text);
+        }
+    }
+}
