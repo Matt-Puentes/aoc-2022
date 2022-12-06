932f2ad7506aa92ccd0a5890c3d8af8e9fafa8a0
Matt Puentes <mpuentes@mitre.org>
2022-12-06 12:05:22 -0500

    Added gag, tests don't clog terminal anymore

--- runner/src/bin/bench.rs
+++ runner/src/bin/bench.rs
@@ -1,9 +1,11 @@
+use gag::Gag;
 use took::{Timer, Took};
 
 const RUNS: usize = 100;
 
 fn main() {
     println!("Benchmarking all days with {} runs...", RUNS);
+    let print_gag = Gag::stdout().unwrap();
 
     let times: Vec<_> = runner::jobs()
         .into_iter()
@@ -21,6 +23,7 @@ fn main() {
             )
         })
         .collect();
+    drop(print_gag);
 
     times.iter().for_each(|t| Took::from_std(t.1).describe(t.0));
     Took::from_std(times.into_iter().map(|(_, t)| t).sum()).describe("everything");
--- runner/src/bin/runner.rs
+++ runner/src/bin/runner.rs
@@ -1,7 +1,10 @@
+use gag::Gag;
 use took::Timer;
 
 fn main() {
     let timer = Timer::new();
+    let print_gag = Gag::stdout().unwrap();
     runner::jobs().into_iter().for_each(|(j, _, i)| j(i));
+    drop(print_gag);
     timer.took().describe("everything");
 }
