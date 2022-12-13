eb8ef0af167271289e5ff16fd41e5d92cf1f687a
Matt Puentes <mpuentes@mitre.org>
2022-12-13 16:42:28 -0500

    re-wrote day 13 with some more idiomatic rust.

--- day_13/src/solutions.rs
+++ day_13/src/solutions.rs
@@ -1,92 +1,79 @@
-use std::cmp::Ordering;
+use std::{cmp::Ordering, str::FromStr};
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Eq, Clone)]
 enum Value {
     Array(Vec<Value>),
-    Num(usize),
+    Num(u8),
 }
 
 // I also did this with serde_json, which was ~3ms faster faster, but I wanted to see if I could deal with the list
 //  stacking logic in rust.
-fn parse_packet(packet_def: &str) -> Value {
-    let mut buff: Vec<char> = vec![];
-    let mut lists: Vec<Value> = vec![];
-    for c in packet_def.chars() {
-        match c {
-            '[' => lists.push(Value::Array(vec![])),
-            ']' => {
-                if let Some(Value::Array(mut list)) = lists.pop() {
-                    if !buff.is_empty() {
-                        let num = Value::Num(
-                            (&buff)
-                                .into_iter()
-                                .collect::<String>()
-                                .parse::<usize>()
-                                .unwrap(),
-                        );
-                        buff.clear();
-                        list.push(num);
+
+// re-written again using https://github.com/SvetlinZarev/advent-of-code/blob/main/2022/aoc-day-13/src/lib.rs
+//  Learned about .take and realized their current list pointer doesn't have any borrow issues
+//  Also moved to FromStr impl so you can just call str.parse::<Value>()
+impl FromStr for Value {
+    type Err = String;
+    fn from_str(packet_def: &str) -> Result<Self, Self::Err> {
+        let mut num: Option<u8> = None;
+        let mut lists: Vec<_> = vec![];
+        let mut currlist: Vec<Value> = vec![];
+        for c in packet_def[1..packet_def.len() - 1].bytes() {
+            match c {
+                b'[' => {
+                    lists.push(currlist);
+                    currlist = vec![];
+                }
+                b']' => {
+                    if let Some(n) = num.take() {
+                        currlist.push(Value::Num(n))
                     }
-                    if let Some(Value::Array(mut plist)) = lists.pop() {
-                        plist.push(Value::Array(list));
-                        lists.push(Value::Array(plist));
+
+                    let list_to_add = Value::Array(currlist);
+                    currlist = lists.pop().unwrap();
+                    currlist.push(list_to_add);
+                }
+                b',' => {
+                    if let Some(n) = num.take() {
+                        currlist.push(Value::Num(n));
+                    }
+                }
+                l => {
+                    if let Some(n) = num {
+                        num = Some((n * 10) + l - b'0')
                     } else {
-                        return Value::Array(list);
-                    }
-                } else {
-                    panic!("No list to end")
-                }
-            }
-            ',' => {
-                if !buff.is_empty() {
-                    let num = Value::Num(
-                        (&buff)
-                            .into_iter()
-                            .collect::<String>()
-                            .parse::<usize>()
-                            .unwrap(),
-                    );
-                    buff.clear();
-                    if let Some(Value::Array(mut list)) = lists.pop() {
-                        list.push(num);
-                        lists.push(Value::Array(list));
+                        num = Some(l - b'0')
                     }
                 }
             }
-            c => buff.push(c),
         }
+        Ok(Value::Array(currlist))
     }
-    panic!("finished function without returning")
 }
 
-fn packet_cmp(packet1: &Value, packet2: &Value) -> Ordering {
-    match (packet1, packet2) {
-        // Arrays compare each of their elements, one-by-one, until one list depletes first.
-        (Value::Array(a1), Value::Array(a2)) => {
-            let mut a1_iter = a1.iter();
-            let mut a2_iter = a2.iter();
-            loop {
-                match (a1_iter.next(), a2_iter.next()) {
-                    (None, None) => break Ordering::Equal,
-                    (None, Some(_)) => break Ordering::Less,
-                    (Some(_), None) => break Ordering::Greater,
-                    (Some(v1), Some(v2)) => match packet_cmp(v1, v2) {
-                        Ordering::Equal => continue,
-                        // If an element within the list decides the ordering, break
-                        r => break r,
-                    },
-                }
-            }
+// re-written again using https://github.com/SvetlinZarev/advent-of-code/blob/main/2022/aoc-day-13/src/lib.rs
+//  realized we're using lexical ordering, so we can take advantage of rust built-ins. an array of values
+//  can use the same comparison methods as the values themselves.
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other) == Ordering::Equal
+    }
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Value {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match (self, other) {
+            (Value::Num(n1), Value::Num(n2)) => n1.cmp(n2),
+            (n1 @ Value::Num(_), Value::Array(n2)) => std::slice::from_ref(n1).cmp(n2.as_slice()),
+            (Value::Array(n1), n2 @ Value::Num(_)) => n1.as_slice().cmp(std::slice::from_ref(n2)),
+            (Value::Array(n1), Value::Array(n2)) => n1.cmp(n2),
         }
-        // I wish I could do this without cloning, but im not sure how I could. cloning an enum(u64) can't be that slow
-        //  though, right?
-        (Value::Num(_), Value::Array(_)) => {
-            packet_cmp(&Value::Array(vec![packet1.clone()]), packet2)
-        }
-        (Value::Array(_), Value::Num(_)) => {
-            packet_cmp(packet1, &Value::Array(vec![packet2.clone()]))
-        }
-        (Value::Num(n1), Value::Num(n2)) => n1.cmp(&n2),
     }
 }
 
@@ -103,11 +90,11 @@ pub fn pt_1(str_input: &str) {
         let blank = lines.next();
 
         // Parse packets
-        let packet1 = parse_packet(packetdef1);
-        let packet2 = parse_packet(packetdef2);
+        let packet1: Value = packetdef1.parse().unwrap();
+        let packet2: Value = packetdef2.parse().unwrap();
 
         // if packet1 < packet 2, the ordering is correct, add the index + 1 to the counter.
-        match packet_cmp(&packet1, &packet2) {
+        match packet1.cmp(&packet2) {
             Ordering::Less => counter += group,
             Ordering::Greater => (),
             Ordering::Equal => panic!("No decision made"),
@@ -124,19 +111,19 @@ pub fn pt_1(str_input: &str) {
 
 pub fn pt_2(str_input: &str) {
     // pt2-specific marker values
-    let marker1 = parse_packet("[[6]]");
-    let marker2 = parse_packet("[[2]]");
+    let marker1 = Value::Num(6);
+    let marker2 = Value::Num(2);
 
     // Build vec of packets
     let mut packets: Vec<Value> = vec![marker1.clone(), marker2.clone()];
     for packetdef in str_input.lines() {
         if !packetdef.is_empty() {
-            packets.push(parse_packet(packetdef));
+            packets.push(packetdef.parse().unwrap());
         }
     }
 
     // Sort vector using packet comparison method
-    packets.sort_unstable_by(|a, b| packet_cmp(a, b));
+    packets.sort_unstable();
 
     // calculate index(marker1) * index(marker2)
     let mut c = 1;
