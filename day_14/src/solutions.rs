806669aa74e01f81c69f6be44eb23af21999e108
Matt Puentes <mpuentes@mitre.org>
2022-12-14 12:18:03 -0500

    move day 14 to floodfill for time savings

--- day_14/src/solutions.rs
+++ day_14/src/solutions.rs
@@ -1,3 +1,5 @@
+use std::collections::VecDeque;
+
 enum Line {
     Horiz((usize, usize), usize),
     Vert(usize, (usize, usize)),
@@ -103,33 +105,26 @@ pub fn pt_1(str_input: &str) {
 pub fn pt_2(str_input: &str) {
     let (mut world, (height_limit, min_col)) = build_world(str_input, true);
     let mut c = 0;
-    'sandSpawn: loop {
-        let mut sand_pos = (0, 500 - min_col);
-        'sandFall: loop {
-            let new_pos = (sand_pos.0 + 1, sand_pos.1);
-            // The sand has hit the floor
-            if new_pos.0 == height_limit {
-                c += 1;
-                world[sand_pos.0][sand_pos.1] = 'o';
-                break 'sandFall;
+
+    // You can just floodfill for pt2, since it'll fill up all possible space
+    // Inspired by (Copied from?) https://www.reddit.com/r/adventofcode/comments/zli1rd/comment/j06ea2y/?utm_source=share&utm_medium=web2x&context=3
+
+    let mut pos_queue: VecDeque<(usize, usize)> = Default::default();
+    pos_queue.push_back((0, 500 - min_col));
+    while let Some(next_pos) = pos_queue.pop_front() {
+        if world[next_pos.0][next_pos.1] == '.' {
+            c += 1;
+            world[next_pos.0][next_pos.1] = 'o';
+
+            let dy = next_pos.0 + 1;
+            if dy == height_limit {
+                continue;
             }
-            if world[new_pos.0][new_pos.1] == '.' {
-                sand_pos = new_pos;
-            } else if world[new_pos.0][new_pos.1 - 1] == '.' {
-                sand_pos = (new_pos.0, new_pos.1 - 1);
-            } else if world[new_pos.0][new_pos.1 + 1] == '.' {
-                sand_pos = (new_pos.0, new_pos.1 + 1);
-            } else {
-                c += 1;
-                world[sand_pos.0][sand_pos.1] = 'o';
-                if sand_pos == (0, 500 - min_col) {
-                    break 'sandSpawn;
-                } else {
-                    break 'sandFall;
-                }
-            }
+
+            pos_queue.push_back((dy, next_pos.1));
+            pos_queue.push_back((dy, next_pos.1 - 1));
+            pos_queue.push_back((dy, next_pos.1 + 1));
         }
     }
-
     println!("Part 2 result: {}", c)
 }
