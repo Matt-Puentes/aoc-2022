a0ba075e247e3031a7c22d3b9dc9e73b2b8b544e
Matt Puentes <mpuentes@mitre.org>
2022-12-15 13:26:08 -0500

    Horribly slow pt 2 solution

--- day_15/src/solutions.rs
+++ day_15/src/solutions.rs
@@ -1,7 +1,7 @@
-const EXAMPLE_Y: i32 = 10;
-const EXAMPLE_MAX: i32 = 20;
-// const INPUT_Y: i32 = 2000000;
-const INPUT_MAX: i32 = 4_000_000;
+const _EXAMPLE_Y: i32 = 10;
+const _EXAMPLE_MAX: i32 = 20;
+const _INPUT_Y: i32 = 2_000_000;
+const _INPUT_MAX: i32 = 4_000_000;
 
 #[derive(Debug)]
 struct Sensor {
@@ -40,8 +40,8 @@ fn build_sensors(str_input: &str) -> Vec<Sensor> {
 pub fn pt_1(str_input: &str) {
     let sensors = build_sensors(str_input);
 
-    let row = EXAMPLE_Y;
-    // let row = INPUT_Y;
+    let row = _EXAMPLE_Y;
+    // let row = _INPUT_Y;
     let mut coverages: Vec<(i32, i32)> = vec![];
     for Sensor { pos, radius } in sensors {
         // the amount of columns a sensor covers at that radius
@@ -70,66 +70,48 @@ pub fn pt_1(str_input: &str) {
 
 pub fn pt_2(str_input: &str) {
     let sensors = build_sensors(str_input);
-    let mut answer: Option<(i32, i32)> = None;
+    let mut answer: Option<i64> = None;
 
     // Never got a good solution. Revisit when you have time and look at existing solutions
 
-    // let max = EXAMPLE_MAX;
-    // let max = INPUT_MAX;
-    // 'outer: for Sensor { pos, radius } in &sensors {
-    //     // get positions outside edge of sensor
-    //     for y in 0..=(radius * 2) {
-    //         let row = (pos.1 - radius) + y;
-    //         let x1 = pos.0 - (radius - (pos.1 - row).abs()) - 1;
-    //         let x2 = pos.0 + (radius - (pos.1 - row).abs()) + 1;
-    //         if y > 0 && y < max && x1 > 0 && x1 < max {
-    //             if sensors
-    //                 .iter()
-    //                 .map(|c| dist((x1, y), c.pos) > c.radius)
-    //                 .all(|b| b)
-    //             {
-    //                 println!("1 FOUND IT {:?}", (x1, y));
-    //                 answer = Some((x1, y));
-    //                 break 'outer;
-    //             }
-    //         }
-    //         if y > 0 && y < max && x2 > 0 && x2 < max {
-    //             if sensors
-    //                 .iter()
-    //                 .map(|c| dist((x2, y), c.pos) > c.radius)
-    //                 .all(|b| b)
-    //             {
-    //                 println!("2 FOUND IT {:?}", (x2, y));
-    //                 answer = Some((x2, y));
-    //                 break 'outer;
-    //             }
-    //         }
-    //     }
-    //     // far edges
-    //     let p1 = (pos.0, pos.1 - radius - 1);
-    //     let p2 = (pos.0, pos.1 + radius + 1);
-    //     if p1.0 > 0 && p1.0 < max && p1.1 > 0 && p1.1 < max {
-    //         if sensors
-    //             .iter()
-    //             .map(|c| dist((p1.0, p1.1), c.pos) > c.radius)
-    //             .all(|b| b)
-    //         {
-    //             println!("1 FOUND IT {:?}", (p1.0, p1.1));
-    //             answer = Some((p1.0, p1.1));
-    //             break 'outer;
-    //         }
-    //     }
-    //     if p2.1 > 0 && p2.1 < max && p2.0 > 0 && p2.0 < max {
-    //         if sensors
-    //             .iter()
-    //             .map(|c| dist((p2.0, p2.1), c.pos) > c.radius)
-    //             .all(|b| b)
-    //         {
-    //             println!("2 FOUND IT {:?}", (p2.0, p2.1));
-    //             answer = Some((p2.0, p2.1));
-    //             break 'outer;
-    //         }
-    //     }
-    // }
+    let range = 0.._INPUT_MAX;
+    // let range = 0.._EXAMPLE_MAX;
+    'outer: for row in range {
+        let mut coverages: Vec<(i32, i32)> = vec![];
+        for Sensor { pos, radius } in &sensors {
+            // the amount of columns a sensor covers at that radius
+            let cov_start = pos.0 - (radius - (pos.1 - row).abs());
+            let cov_end = pos.0 + (radius - (pos.1 - row).abs()) + 1;
+            if cov_start < cov_end {
+                coverages.push((cov_start, cov_end));
+            }
+        }
+
+        // merge overlapping coverages
+        coverages.sort_unstable_by(|a, b| a.0.cmp(&b.0));
+        let mut merged_coverages: Vec<(i32, i32)> = vec![coverages[0].clone()];
+        for i in 1..coverages.len() {
+            let cov = coverages[i].clone();
+            let idx = merged_coverages.len() - 1;
+            if cov.0 >= merged_coverages[idx].0 && cov.0 <= merged_coverages[idx].1 {
+                // println!(
+                //     "coverage {:?} eats coverage {:?}",
+                //     merged_coverages[idx], cov
+                // );
+                merged_coverages[idx].1 = cov.1.max(merged_coverages[idx].1);
+            } else {
+                // println!("cov gets pushed onto stack");
+                merged_coverages.push(cov);
+            }
+        }
+        if merged_coverages.len() == 2 {
+            println!("Found it: {}, {}", merged_coverages[0].1, row);
+            answer = Some((merged_coverages[0].1 as i64 * 4000000 as i64) + row as i64);
+            break 'outer;
+        }
+
+        // println!("{}: {:?}", row, merged_coverages);
+    }
+
     println!("Part 2 result: {:?}", answer.expect("no answer for pt 2"))
 }
